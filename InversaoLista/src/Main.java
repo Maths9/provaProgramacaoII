7c044ca79f7c2bbc23d33aeefa7b7a3474871c2d
maths9 <devilcaos110@gmail.com>
2023-12-04 16:16:43 -0300

    Prova

--- InversaoLista/src/Main.java
+++ InversaoLista/src/Main.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        int numero_A;
+        int numeros;
         Scanner scan = new Scanner(System.in);
         System.out.println("Escreva Seus Números:");
-        numero_A = scan.nextInt();
-
+        numeros = scan.nextInt();
         scan.close();
 
-        for( int i = 0; i+){
+        for( int i = 0; i++){
             if( i%2 == 1){
                 System.out.println(i);
             }
